fef24615cd026f123232df5d5adbf0a91edca218
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2023-05-20 13:46:38 +0530

    fixed assets

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,7 +3,7 @@ import './Login.css';
 export default function Login() {
     return (
         <div className='login'>
-            <img src="/assets/Spotify_Logo_CMYK_White.png" alt="" />
+            <img src="/src/assets/Spotify_Logo_CMYK_White.png" alt="" />
             <a className='not-selectable'>Login with Spotify</a>
         </div>
     );
