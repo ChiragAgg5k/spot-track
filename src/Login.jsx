3572c24996a07256aede2ddd6bec2e25aa05fe64
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2023-05-20 13:56:17 +0530

    fixed importing of logo

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,10 @@
 import './Login.css';
+import spotify_logo from './assets/Spotify_Logo_CMYK_White.png';
 
 export default function Login() {
     return (
         <div className='login'>
-            <img src="/src/assets/Spotify_Logo_CMYK_White.png" alt="" />
+            <img src={spotify_logo} alt="Spotify Logo" id='spotify-logo' />
             <a className='not-selectable'>Login with Spotify</a>
         </div>
     );
